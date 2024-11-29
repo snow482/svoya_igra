767338b902e98632d7688226ab0dc3230b138023
Mukhammad <jonbola1999@gmail.com>
2024-11-29 11:31:56 +0300

    add categoryes

---
+++ server/controllers/CategoryController.js
@@ -0,0 +1,19 @@
+const CategoryService = require("../service/Category.service");
+
+exports.getAllCategoryController = async (req, res) => {
+  try {
+    const categories = await CategoryService.getAllCategory();
+    res.status(200).json({ message: "success", categories });
+  } catch (error) {
+    res.status(500).json({ message: error.message, categories: [] });
+  }
+};
+
+exports.getOneCategoryController = async (req, res) => {
+  try {
+    const category = await CategoryService.getOneCategory();
+    res.status(200).json({ message: "Success", category });
+  } catch (error) {
+    res.status(500).json({ message: error.message, category: {} });
+  }
+};
--- server/routes/api.routes.js
+++ server/routes/api.routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const apiAuthRouter = require("./api.auth.routes");
 const apiQuestionRouter = require("./api.questions.routes");
+const apiCategoryRouter = require("./api.categoryes.routes");
 
 router.use("/auth", apiAuthRouter);
 router.use("/questions", apiQuestionRouter);
+router.use("/categoryes", apiCategoryRouter);
 
 module.exports = router;
