0c9bfddad16e687eae3494838fc08579f1a26be0
snow482 <a.dolin125@gmail.com>
2024-11-29 15:10:29 +0300

    questionUpdate fix

--- client/src/pages/QuestionUpdatePage/ui/QuestionUpdate.tsx
+++ client/src/pages/QuestionUpdatePage/ui/QuestionUpdate.tsx
@@ -4,27 +4,29 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "@/shared/hooks/rtkHooks";
 import { updateQuestion } from "@/entities/question/model/questionThunk";
 
-export function QuestionUpdate() { 
+export function QuestionUpdate() {
   const dispatch = useAppDispatch()
   const { questions } = useAppSelector((state) => state.questions)
+  const { user } = useAppSelector((state) => state.user)
+  
   const { id } = useParams()
   const navigate = useNavigate()
 
   const question = questions.find((el) => el.id === Number(id))
+  const [answer, setAnswer] = useState<string>(question?.answer || '');
 
-  const [title, setTitle] = useState<string>(question?.description || '');
-  const [author, setAuthor] = useState<string>(question?.author || '');
-  const [pages, setPages] = useState<number>(question?.pages || 0);
-  const [category_id, setCategoryId] = useState<number>(question?.category_id || 0);
-
-  const handleUpdate = async (event: React.FormEvent) => {
+  const handleAnswer = async (event: React.FormEvent) => {
     event.preventDefault();
     try {
       if (!question) {
         return
       }
+      if (answer === question.answer) {
+        dispatch(updateUser(user.id));
+        return
+      }
     
-      dispatch(updateQuestion({id: Number(id), title, author, pages, category_id}));
+      
       navigate('/questions')
     } catch (error) {
       console.error("Error deleting question:", error);
@@ -34,40 +36,15 @@ export function QuestionUpdate() {
   return (
     <section>
       <div className="question-update-item">
-        <form onSubmit={handleUpdate}>
-          <p>Название: </p>
+        <form onSubmit={handleAnswer}>
+          <p>Ответ: </p>
           <input
             type="text"
-            onChange={({ target }) => setTitle(target.value)}
-            defaultValue={title}
+            onChange={({ target }) => setAnswer(target.value)}
             required
             placeholder="Question title"
           />
-          <p>Автор: </p>
-          <input
-            type="text"
-            onChange={({ target }) => setAuthor(target.value)}
-            defaultValue={author}
-            required
-            placeholder="Question author"
-          />
-          <p>Количество страниц: </p>
-          <input
-            type="number"
-            onChange={({ target }) => setPages(Number(target.value))}
-            defaultValue={pages}
-            required
-            placeholder="Pages"
-          />
-          <p>Категория: </p>
-          <input
-            type="text"
-            onChange={({ target }) => setCategoryId(Number(target.value))}
-            defaultValue={category_id}
-            required
-            placeholder="Category"
-          />
-          <button>Update question</button>
+          <button>Answer</button>
         </form>
       </div>
     </section>
